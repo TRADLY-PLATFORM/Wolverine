19dde94eb2b4a2658c7fbae6d687ec70890a4891
Dipesh Saini <mannusaini0001@gmail.com>
2021-08-24 19:43:45 +0530

    add price filter

--- UI/Event/EventList.js
+++ UI/Event/EventList.js
@@ -164,6 +164,10 @@ export default class EventList extends Component {
         let dObjc = objc['category']
         queryParams = queryParams + `&category_id=${dObjc['id']}`;
       }
+      if (objc['price']) {
+        let dObjc = objc['price']
+        queryParams = queryParams + `&price_from=${dObjc['from']}&price_to=${dObjc['to']}`;
+      }
     }
     this.state.filterArray = data
     this.state.params = queryParams;
