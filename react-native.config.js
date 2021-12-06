1b070e0fefde530f4d7387e2f7ee2f2f97ea263a
Dipesh Saini <mannusaini0001@gmail.com>
2021-12-06 16:44:13 +0530

    fixed filter issue

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dependencies: {
+    '@jesster2k10/react-native-range-slider': {
+      platforms: {
+        android: {
+          packageImportPath:
+            'import com.jesster2k10reactnativerangeslider.ReactNativeRangeSliderPackage;',
+        },
+      },
+    },
+  },
+}
